3f4df0bc1fc0342d4818960c54059c7cf7dfc111
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-12-07 11:58:17 +0000

    Working version
    - Fix bug:
    -- found one misspelling;
    -- found mistake in for loop

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,11 +1,11 @@
 function getReceipt() {
     // This initializes our string so it can get passed from
     // function to function, growing line by line into a full reseipt
-    var text1= "<h3>You Ordered:</h3>";
+    var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0;
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
-    for (var i = 0; sizeArray.length; i++) {
+    for (var i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
             text1 = text1+selectedSize+"<br>";
@@ -26,8 +26,7 @@ function getReceipt() {
     console.log(selectedSize+" = $"+sizeTotal+".00");
     console.log("size text1: "+text1);
     console.log("subtotal: $"+runningTotal+".00");
-    //these variables will get passed on to each function
-    getTopping(runningTotal,text1);
+    getTopping(runningTotal,text1); //these variables will get passed on to each function
 };
 
 function getTopping(runningTotal,text1) {
@@ -52,6 +51,6 @@ function getTopping(runningTotal,text1) {
     console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
     console.log("topping text1: "+text1);
     console.log("Purchase Total: "+"$"+runningTotal + ".00");
-    document.getElementsById("showText").innerHTML=text1;
+    document.getElementById("showText").innerHTML=text1;
     document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
 };
