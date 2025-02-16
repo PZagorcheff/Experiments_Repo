eefcc78b4a225e40a05c33853d317b4b7fc20e84
Petar Zagorchev <petar.zagorcheff@gmail.com>
2025-02-16 11:29:12 +0000

    Initial upload
    Version 1 of PZ Portfolio Website

---
+++ PZ_Portfolio_Website/js/portfolio.js
@@ -0,0 +1,18 @@
+// These functions open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Creates close the contact form when the user clicks off of it
+document.addEventListener("click", function(event){
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+        closeForm()
+    }
+}, false )
